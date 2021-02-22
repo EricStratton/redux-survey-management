0bad4d284221cca329944dfbcff30364890d1a85
Eric Stratton <strattonericj@gmail.com>
2021-02-22 13:30:51 -0800

    add basic survey file
    Co-authored-by: Austin Schrader <austin.jschrader@gmail.com>

---
+++ src/components/Survey.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Survey(props) {
+  return ( 
+    <>
+    </>
+  )
+}
+
+Survey.propTypes = {
+  id: PropTypes.string,
+}
+
+export default Survey;
