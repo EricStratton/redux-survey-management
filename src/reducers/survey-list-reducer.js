a1656fd3da845d18d8514f3d82b7f80240c123e1
Eric Stratton <strattonericj@gmail.com>
2021-02-23 09:15:34 -0800

    pass first test
    co-authored-by: Joe Nero <josephnero111@gmail.com>
    Co-authored-by: Austin Schrader <austin.jschrader@gmail.com>

--- src/reducers/survey-list-reducer.js
+++ src/reducers/survey-list-reducer.js
@@ -1,7 +1,15 @@
 export default (state = {}, action) => {
-  // const { name, topic, id } = action;
-  // switch(action.type) {
-  //   case "ADD_KEG":
-  //     return Object.assign({}, state, )
-  // }
-}
+  const { name, topic, id } = action;
+  switch(action.type) {
+    case "ADD_KEG":
+      return Object.assign({}, state, {
+        [id]: {
+          name,
+          topic,
+          id
+        }
+      });
+    default:
+      return state;
+  }
+};
