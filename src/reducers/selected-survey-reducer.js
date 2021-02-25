ea9998e5b77d4c553456a4d90f034cc888604b0e
Eric Stratton <strattonericj@gmail.com>
2021-02-25 09:37:08 -0800

    polish up reducers
    Co-authored-by: Joe Nero <josephnero111@gmail.com>
    Co-authored-by: Austin Schrader <austin.jschrader@gmail.com>

--- src/reducers/selected-survey-reducer.js
+++ src/reducers/selected-survey-reducer.js
@@ -1,3 +1,17 @@
+import { createStore } from "redux";
+
 export default (state = null, action) => {
-  return state;
+  const { name, topic, id } = action;
+  switch(action.type) {
+    case c.SELECT_SURVEY:
+      return {
+        name,
+        topic,
+        id
+      }
+    case c.DESELECT_SURVEY:
+      return null;
+    default:
+      return state;
+  }
 }
